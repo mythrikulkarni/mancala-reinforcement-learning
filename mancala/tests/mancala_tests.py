778f9ea9015a34a2bc489661ffb44d52a9f57e59
Matt Gray <blampz@hotmail.com>
2017-11-30 17:55:56 -0500

    Added test for the bug case seen in testing

--- mancala/tests/mancala_tests.py
+++ mancala/tests/mancala_tests.py
@@ -8,20 +8,16 @@ Created on Thu Nov 30 16:22:50 2017
 import unittest
 from mancala.mancala import Mancala
 
-# Test case
-board_layout = [1,0,1,0,0,0,22,0,0,1,0,0,0,23]
-expected_move = (3,2)
-expected_result = [0,0,0,0,0,0,23,0,0,0,0,0,0,25]
-
 class FinalMove(unittest.TestCase):
     
     def setUp(self):
-        environment = Mancala()
+        pass
     
     # Bug seen at end of game when capture and game over occur in same move
     def test_bug1(self):
+        environment = Mancala()
         environment.pockets = [1,0,1,0,0,0,22,0,0,1,0,0,0,23]
-        environment.simulate_move(3,2)
+        environment.simulate_move(9,2)
         expected_result = [0,0,0,0,0,0,23,0,0,0,0,0,0,25]
         
         self.assertEqual(expected_result, environment.pockets)
