16d4aff9d67b20329cfec0c446be2bce507e6ae8
Matt Gray <blampz@hotmail.com>
2017-12-08 17:18:31 -0500

    Fixed issue with loading model for additional training instead of starting with new model

--- mancala/train_mancala_agent.py
+++ mancala/train_mancala_agent.py
@@ -5,12 +5,19 @@ Created on Thu Nov 30 10:40:03 2017
 @author: blamp
 """
 
+import os
 import logging
 from mancala import Mancala
+from agent import Agent
 
 def train_agent(n_games=1, games_per_checkpoint=1, model_save_path='model/mancala_agent.pkl'):
     
-    environment = Mancala()
+    # If model already exists, expand on it, otherwise start fresh
+    if os.path.exists(model_save_path):
+        loaded_agent = Agent(load_agent_path = model_save_path)
+        environment = Mancala(loaded_agent)
+    else:
+        environment = Mancala()
     
     while n_games>0:
         environment.play_game(reinforcement_learning=True)
@@ -18,6 +25,7 @@ def train_agent(n_games=1, games_per_checkpoint=1, model_save_path='model/mancal
         if n_games%games_per_checkpoint == 0:
             environment.mancala_agent.save_agent(model_save_path)
             logging.info('Saved RL Agent Model!')
+            print('Remaining Games: ', n_games)
         n_games -= 1
         
     # Save final agent model
@@ -27,6 +35,6 @@ def train_agent(n_games=1, games_per_checkpoint=1, model_save_path='model/mancal
 
 
 if __name__ == "__main__":
-    environment = train_agent(n_games = 1000, games_per_checkpoint=100)
+    environment = train_agent(n_games = 1, games_per_checkpoint=25000)
     
     
